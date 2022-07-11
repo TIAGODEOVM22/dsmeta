1346a33f88cf7a87eb0a3e2f7782b74238d692e3
tiago <tiagodeoliveiravillalvamorel@gmail.com>
2022-07-11 19:31:12 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperio/desmeta/DesmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperio.desmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
