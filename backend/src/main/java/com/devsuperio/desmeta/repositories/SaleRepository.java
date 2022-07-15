c2ac81cb605d0ea5dcc0a729b34019f6da517db3
tiago <tiagodeoliveiravillalvamorel@gmail.com>
2022-07-15 12:25:08 -0300

    API test

---
+++ backend/src/main/java/com/devsuperio/desmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperio.desmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperio.desmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/devsuperio/desmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.devsuperio.desmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperio.desmeta.entities.Sale;
+import com.devsuperio.desmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public List <Sale> findSales(){
+		return repository.findAll();
+		
+	}
+}
